0a07cf1f81ea0d2a66fec84556978fc229cb66a5
rahulpunase <rahulpunase@gmail.com>
2022-02-20 17:15:16 +0530

    Added internationalization. Added multiple tests.

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import App from './App';
+import store from "./store/store";
+import {Provider} from "react-redux";
+import {I18nProvider} from "./i18n/i18n.provider";
+
+
+
+describe('App component should', () => {
+	it('render without crashing', () => {
+		render(
+			<Provider store={store}>
+				<I18nProvider>
+					<App />
+				</I18nProvider>
+			</Provider>,
+		);
+	});
+});
---
+++ src/components/header/__test__/header.test.tsx
@@ -0,0 +1,34 @@
+import React, {ReactNode} from 'react';
+import {render as rtlRender , screen, fireEvent} from '@testing-library/react';
+import Header from './header';
+import {Provider} from "react-redux";
+import store from "../../store/store";
+import {I18nProvider} from "../../i18n/i18n.provider";
+
+const render = (component: ReactNode) => rtlRender(
+	<Provider store={store}>
+		<I18nProvider>
+			{component}
+		</I18nProvider>
+	</Provider>
+);
+
+describe('Header Component should', () => {
+	it(' renders correctly', () => {
+		render(<Header/>);
+	});
+	it (' have Centime logo', () => {
+		render(<Header/>);
+		const logo = screen.getByAltText('centime-logo');
+		expect(logo).toBeInTheDocument();
+		expect(logo).toHaveAttribute('src', 'https://uploads-ssl.webflow.com/615f40af7a135839ad16cf09/615f40af7a1358d94716cf4e_logo2.svg');
+	});
+
+	it(' should work on onChange', () => {
+		render(<Header />)
+		const languageLabel = screen.getByText(/Change Language:/i);
+		const select = screen.getByTestId('change-language');
+		fireEvent.change(select, { target: { value: 'de-de' } });
+		expect(languageLabel).toHaveTextContent('Sprache ändern:');
+	})
+});
